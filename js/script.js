f308c7bd7ac2803cb64dcd6fa26267cbb095de31
Charles Joma <almprincecharles@yahoo.com>
2022-09-12 17:25:51 +1000

    Mobile friendly

--- js/script.js
+++ js/script.js
@@ -1,25 +1,147 @@
 
 
-const button = $('.btn');
+const button = $('.game-button');
 const img = $('img');
 let turns = 0 ;
+let xScore = 0;
+let yScore = 0;
 
 const buttonsContainer = $('.buttons-container');
 
-const set0 = '<img class="o" src="./images/pepe2.png">';
-const set0win = '<img class="o" src="./images/pepeGold.png">';
-const setX = '<img class="x" src="./images/sassy2.png">'
-const setXwin = '<img class="x" src="./images/SassyGold.png">'
+let set0 = '<img class="o " id="img-square" src="./images/blue.png">';
+let set0win = '<img class="o " id="img-square" src="./images/pepeGold.png">';
+let setX = '<img class="x " id="img-square" src="./images/robinhood.png">'
+let setXwin = '<img class="x " id="img-square" src="./images/SassyGold.png">'
 const gameOver = $('.gameOver')
 const gameOverText = $('.gameOverText')
-const Ywins = $('.Xwins');
-const Xwins = $('.Ywins');
+// const Ywins = $('.Xwins');
+// const Xwins = $('.Ywins');
+
+let robinhood = $('.robinhood');
+let blue = $('.blue');
+let pepe = $('.pepe');
+let sassy = $('.sassy');
+let charButton = $('.char-button')
 
 const resetButton = $('.reset')
 
+const characterSelect = () => {
+
+    console.log($('.sassy-score-div .font2').text())
+    let charactersSelected = [];
+
+    let charTurn = 0;
+    
+    const checkSelected = () => {
+        if(charTurn < 2){
+            button.attr('disabled', true)
+        } else if(charTurn === 2){
+            charButton.attr('disabled', true)
+            button.attr('disabled', false)
+        } 
+    }
+    let player1Name = $('')
+  
+    robinhood.on('click', function(){
+        charTurn++
+        if(charTurn <= 1){
+            robinhood.attr('disabled', true);
+            $('.player1').text('Robinhood');
+            charactersSelected.push('robinhood')
+            set0 = '<img class="o" id="img-square" src="./images/robinhood.png">';
+            set0win = '<img class="o" id="img-square" src="./images/robinhood.png">';
+            console.log(robinhood)
+            checkSelected();
+        } else if(charTurn === 2){
+            robinhood.attr('disabled', true);
+            $('.player2').text('Robinhood');
+            charactersSelected.push('robinhood')
+            setX = '<img class="x" id="img-square" src="./images/robinhood.png">';
+            setXwin = '<img class="x" id="img-square" src="./images/robinhood.png">';
+            console.log(robinhood)
+            checkSelected();
+        }
+       
+    });
+
+    blue.on('click', function(){
+        charTurn++ 
+        if(charTurn <= 1){
+            blue.attr('disabled', true);
+            $('.player1').text('Blue')
+            charactersSelected.push('blue');
+            set0 = '<img class="o" id="img-square" src="./images/blue.png">';
+            set0win = '<img class="o" id="img-square" src="./images/blue.png">';
+            console.log(blue)
+            checkSelected();
+        } else if(charTurn === 2){
+            blue.attr('disabled', true);
+            $('.player2').text('Blue')
+            charactersSelected.push('blue');
+            setX = '<img class="x" id="img-square" src="./images/blue.png">';
+            setXwin = '<img class="x" id="img-square" src="./images/blue.png">';
+            console.log(blue)
+            checkSelected();
+        }
+       
+    });
+
+    sassy.on('click', function(){
+        charTurn++
+        if(charTurn <= 1){
+            sassy.attr('disabled', true);
+            $('.player1').text('Sassy')
+            charactersSelected.push('sassy')
+            set0 = '<img class="o" id="img-square" src="./images/sassy2.png">';
+            set0win = '<img class="o" id="img-square" src="./images/sassyGold.png">';
+            console.log(sassy)
+            checkSelected();
+        } else if(charTurn === 2 ){
+            sassy.attr('disabled', true);
+            $('player2').text('Sassy');
+            charactersSelected.push('sassy')
+            setX = '<img class="x" id="img-square" src="./images/sassy2.png">';
+            setXwin = '<img class="x" id="img-square" src="./images/sassyGold.png">';
+            console.log(sassy)
+            checkSelected();
+        }
+
+        
+    });
+
+    pepe.on('click', function(){
+        charTurn++
+        if(charTurn <= 1){
+        pepe.attr('disabled', true);
+        $('.player1').text('Pepe');
+        charactersSelected.push('pepe');
+        set0 = '<img class="o" id="img-square" src="./images/pepe2.png">';
+        set0win = '<img class="o" id="img-square" src="./images/pepeGold.png">';
+        console.log(pepe)
+        } else if(charTurn === 2){
+        pepe.attr('disabled', true);
+        $('.player2').text('Pepe');
+        charactersSelected.push('pepe');
+        setX = '<img class="x" id="img-square" src="./images/pepe2.png">';
+        setXwin = '<img class="x" id="img-square" src="./images/pepeGold.png">';
+        console.log(pepe)
+        }
+
+    });
+
+}
 
 
-button.on('click', function(){
+
+const start = () => {
+
+    $('.player1').text('')
+    $('.player2').text('')
+
+    characterSelect();
+  
+
+    button.on('click', function(){
     turns++
 
     const imgXO = turns % 2 === 0;
@@ -57,18 +179,20 @@ button.on('click', function(){
     const Xwon = function(){
         button.attr('disabled', true)
         gameOver.addClass('visible');
-        gameOverText.html(`${setXwin} wins`)
-        gameOverText.css({'transform': `translateX(${150}%)`})
+        gameOverText.html(`${setXwin} wins`);
+        // gameOverText.css({'transform': `translateX(${150}%)`})
         buttonsContainer.addClass('invisible')
-        $('.XkeepingCount').append('I')
+        xScore++
+        $('.XkeepingCount').append(xScore)
     }
 
     const Ywon = function(){
-        $('.OkeepingCount').append('I')
+        yScore++
+        $('.OkeepingCount').append(yScore)
         button.attr('disabled', true)
         gameOver.addClass('visible');
         gameOverText.html(`${set0win} wins`)
-        gameOverText.css({'transform': `translateX(${150}%)`})
+        // gameOverText.css({'transform': `translateX(${150}%)`})
         buttonsContainer.addClass('invisible')
     }
     const draw = function(){
@@ -86,8 +210,9 @@ button.on('click', function(){
         } else if($('.btn').attr('disabled') && turns >= 9){
             draw()
         } ;
-
-});
+    })
+};
+start();
 
 resetButton.on('click', function(){
     $('button, btn').attr('disabled', false);
